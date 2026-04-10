a03aba0481cd2e3bea389cfbb56181a44235f500
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-04-10 23:30:16 +0530

    latest/best collection and policy section completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,7 +10,9 @@ const Navbar = () => {
 
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-      <img src={assets.logo} className='w-35' alt="" />
+      <Link to='/'>
+       <img src={assets.logo} className='w-35 cursor-pointer' alt="" />
+      </Link>
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700 '>
 
          <NavLink to='/' className='flex flex-col items-center gap-1'>
