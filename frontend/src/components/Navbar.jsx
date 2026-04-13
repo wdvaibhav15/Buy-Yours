3c8fe81b0bf1e9eae48408b628067f788465e967
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-04-13 11:01:07 +0530

    login/ sign up ui completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const{setShowSearch, getCartCount} = React.useContext(ShopContext);
        <div className="flex items-center gap-6">
             <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 curor-pointer' alt="" />
             <div className="group relative">
-                 <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
+                 <Link to="/Login"><img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" /></Link>
                  <div className=" group-hover:block hidden absolute dropdown-menu right-0 pt-4">
                      <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
                         <p className=' cursor-pointer hover:text-black'>My Profile</p>
