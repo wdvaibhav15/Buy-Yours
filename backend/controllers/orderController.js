347c538e22be6ec47308aa7ab8bdc304311fa759
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-04-23 17:08:21 +0530

    payment gayway stripe is successfully added in website

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,15 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
+import Stripe from 'stripe';
+
+// global variables
+const currency = "inr"
+const deliveryCharges = 10
+
+
+// payment gateway initialized
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
 
 // placing order using cash on delivery method
 const placeOrder = async (req, res) => {
@@ -31,12 +41,60 @@ const placeOrder = async (req, res) => {
 // placing order using Stripe method
 const placeOrderStripe = async (req, res) => {
   try {
-    res.json({ success: false, message: "Stripe not implemented yet" });
+    const { userId, items, amount, address } = req.body;
+    const { origin } = req.headers;
+
+    const orderData = {
+      userId,
+      items,
+      amount,
+      address,
+      paymentMethod: "Stripe",
+      payment: false,
+      date: Date.now()
+    };
+
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+
+    const line_items = items.map((item) => ({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: item.name
+        },
+        unit_amount: item.price * 100
+      },
+      quantity: item.quantity
+    }));
+
+    line_items.push({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: "Delivery Charges"
+        },
+        unit_amount: deliveryCharges * 100
+      },
+      quantity: 1
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+      line_items,
+      mode: "payment",
+    });
+
+    res.json({ success: true, session_url: session.url });
+
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
   }
 };
+
+
 
 // placing order using Razorpay method
 const placeOrderRazorpay = async (req, res) => {
